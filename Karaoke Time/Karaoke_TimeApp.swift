df7970785ea22d711c09e43c6232670fabfc52a0
Van Nguyen <nguyen.van.t@gmail.com>
2026-01-28 17:52:27 -0800

    Initial Commit

---
+++ Karaoke Time/Karaoke_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Karaoke_TimeApp.swift
+//  Karaoke Time
+//
+//  Created by Van Nguyen on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct Karaoke_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Karaoke TimeTests/Karaoke_TimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Karaoke_TimeTests.swift
+//  Karaoke TimeTests
+//
+//  Created by Van Nguyen on 1/28/26.
+//
+
+import Testing
+@testable import Karaoke_Time
+
+struct Karaoke_TimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
